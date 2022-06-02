b427c044fc005d79b923fcb50b6d0fb6daa973b9
ingvildriska <iri018@uib.no>
2022-06-02 16:23:56 +0200

    Changed stuff

--- pretty_penguins.r
+++ pretty_penguins.r
@@ -5,12 +5,14 @@ library(tidyverse)
 
 dat <- penguins
 
+# Where the penguins live:)
 dat %>% ggplot() + 
   aes(x = species, y = island) +
   geom_point() + 
   theme_bw()
 
+# penguin weight <3
 dat %>% ggplot() + 
-  aes(x = species, y = body_mass_g) +
-  geom_point() + 
+  aes(x = species, y = body_mass_g, fill = species) +
+  geom_histogram() + 
   theme_bw()
