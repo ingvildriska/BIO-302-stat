9d83b2b64c4bdf2836d854913d353399b00e3649
ingvildriska <iri018@uib.no>
2022-06-02 15:35:50 +0200

    Initial commit

---
+++ lecture_1.r
@@ -0,0 +1,19 @@
+# first lecture :)
+# 02.06.2022
+
+# random number between 0 and 1
+# got under 0.05 after 20 tries:)
+runif(1)
+
+
+
+# install.packages("remotes")
+# remotes::install_github("biostats-r/biostats.apps")
+library(biostats.tutorials)
+
+publication_bias_app()
+
+
+# github
+
+install.packages("usethis")
